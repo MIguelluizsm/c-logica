7fe18c6dba6d4aae638dbdbd8344400e601608cc
MIguelluizsm <xxx987654331@gmail.com>
2026-01-06 18:39:44 -0300

    gcc media.c -o media ./media

--- media-notas/media.c
+++ media-notas/media.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+
+int main() {
+
+    float n1, n2, n3, media;
+    int repetir;
+
+    do {
+
+        printf("=== Sistema de Media Escolar ===\n");
+
+        printf("Digite a primeira nota: ");
+        scanf("%f", &n1);
+
+        printf("Digite a segunda nota: ");
+        scanf("%f", &n2);
+
+        printf("Digite a terceira nota: ");
+        scanf("%f", &n3);
+
+        media = (n1 + n2 + n3) / 3;
+
+        printf("\nMedia final: %.2f\n", media);
+
+        if (media >= 7) {
+            printf("Situacao: APROVADO\n");
+        } else if (media >= 5) {
+            printf("Situacao: RECUPERACAO\n");
+        } else {
+            printf("Situacao: REPROVADO\n");
+        }
+
+        printf("\nDeseja calcular outra media?\n");
+        printf("Digite 1 para SIM ou 0 para NAO: ");
+        scanf("%i", &repetir);
+
+        printf("\n");
+
+    } while (repetir == 1);
+
+    printf("Programa encerrado.\n");
+
+    return 0;
+}
 
